8ba9172c7588f8db06dd39820976b41c46ae176a
Peter Teuben <teuben@gmail.com>
2021-01-15 15:44:35 -0500

    quick setup for pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#from distutils.core import setup, Extension
+from setuptools import setup, find_packages
+
+setup(name='maskmoment',
+      version='1.0',
+      description='making moments from radio cubes',
+      author='Tony Wong',
+      packages=['.'],
+)	
